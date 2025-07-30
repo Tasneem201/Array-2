9023e13b51258474604c01ed46d271191a143ec3
Tasneem201 <tasneemibrahim425@gmail.com>
2025-07-30 21:19:36 +0400

    Update has77.java

--- has77.java
+++ has77.java
@@ -1,4 +1,7 @@
-for (int i = 0; i < nums.length; i++) {
+public class Has77 {
+
+    public static boolean has77(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
             if (i + 1 < nums.length && nums[i] == 7 && nums[i + 1] == 7) {
                 return true;
             }
@@ -7,3 +10,12 @@ for (int i = 0; i < nums.length; i++) {
             }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(has77(new int[]{1, 7, 7}));        // true
+        System.out.println(has77(new int[]{1, 7, 1, 7}));     // true
+        System.out.println(has77(new int[]{1, 7, 1, 1, 7}));  // false
+    }
+}
+
